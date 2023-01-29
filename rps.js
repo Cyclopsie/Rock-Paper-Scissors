3a70d9f98601b32c9702baacb6192765e7cdac16
Cyclopsie <eyepurplecat@gmail.com>
2023-01-29 03:27:05 -0800

    fix scoring and ending game

--- rps.js
+++ rps.js
@@ -3,6 +3,8 @@ const buttAll = document.querySelectorAll('button');
 let computerScore = 0;
 let drawScore = 0;
 let playerScore = 0;
+let gameScore = 0;
+
 
 let pWinner = "You are the Champion!";
 let cWinner = "You are DEFEATED!!!!";
@@ -11,104 +13,104 @@ let dWinner = "The game is a DRAWWWWW!";
 //shouldn't change
 const computerChoice = ['rock', 'paper', 'scissors']; 
 
-//let computerSelection = '';
+//identifying playerSelection with Button ID
 
-//computer selection
-//function computerPlay() {
-  //  computerChoice[Math.round(Math.random() * 2)];
-   // console.log(computerSelection);
-//}
-
-function stopButton() {
-    buttAll.forEach((button) => {
-        button.disabled
-    });
-}
-
-
-
-
-
-
-////start of game() funtion);
-function game() {
-    /////for loop game of 5////
-    for ( let i = 1; i < 6; i++) {
-        //console.log(computerScore + "\n" + drawScore + "\n" + playerScore + "\n" + i + "\n");
-
-        let playerSelection = '';
-
-        
-        //button selection
-        console.log(playerSelection);
-        
-        let computerSelection = computerChoice[Math.round(Math.random() * 2)];
-        console.log(computerSelection);
-        
-                        
-        if (
-            ((playerScore >= 3) && (i == 5)) || 
-            ((playerScore > computerScore) && (i == 5))){
-                stopButton();
-                return pWinner;
-        } else if (
-            ((computerScore >= 3) && (i == 5)) ||
-            ((playerScore < computerScore) && (i == 5))){
-                stopButton();
-                return cWinner;
-        } else if (
-            ((drawScore >= 3) && (i == 5)) ||
-            ((playerScore = computerScore) && (i == 5)) ||
-            ((drawScore > playerScore) && (drawScore > computerScore) && (i == 5))){
-                stopButton();
-                return dWinner;
-        } else {  
-            //play another round
-            playRound(playerSelection, computerSelection);
-        }
-    }  
-}
-/////end of game() and loop////
-                  
+buttAll.forEach((button) =>
+button.addEventListener("click", function(e) {
+    //console.log(this);
+   // console.log(button.id);
+    playerSelection = this.id;
+    console.log("Player Selection = " + playerSelection);
+    
+    updateC();
+    updateD();
+    updateP();
+    //checking the score before using the button clicked
+    if (drawScore >= 3 && gameScore == 5) {
+            stopGame();
+            return dWinner;
+   
+    } else if (((playerScore >= 3) && (gameScore == 5)) || 
+        ((playerScore > computerScore) && (gameScore == 5))){
+            stopGame();
+            return pWinner;
+                
+    } else if ((computerScore >= 3 && gameScore == 5) ||
+        ((playerScore < computerScore) && gameScore == 5)){
+            stopGame();
+            return cWinner;
+                
+    } else {
+        //not enought rounds allows for button
+        playRound(playerSelection);
+    }
+})
+);
 
 /////beginning of playRound///////
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
+    //computer selection
+    
+    let computerSelection = computerChoice[Math.round(Math.random() * 2)];
+    console.log("Computer = " + computerSelection);
+    document.querySelector('.round').textContent = "Your weapon is " + playerSelection.toUpperCase() + " against " + (computerSelection.toUpperCase() )+ ".";
+    
+    //round rules
     if (playerSelection === computerSelection){
             drawScore++; //TIE
+            gameScore++;
+            
+            updateD();
+           
     } else if (
         (playerSelection === 'rock' && computerSelection === 'scissors') || 
         (playerSelection === 'scissors' && computerSelection === 'paper') || 
         (playerSelection === 'paper' && computerSelection === 'rock')){
         //Player Wins, computer loses
             playerScore++; 
+            updateP();
+            
+            gameScore++;
+           
     } else {
             computerScore++; 
+            gameScore++;
+            
+            updateC();
+           
     } 
-                
+    
+    
+    
     console.log("Player score is " + playerScore);
                 
     console.log("Computer score is " + computerScore);
                 
-    console.log("Tie rounds is " + drawScore);       
+    console.log("Tie rounds is " + drawScore);     
+    
+    console.log("Game round is " + gameScore);
 }
 ///////end of playRound//////
 
-//have to call the function to start the game
 
 
-//assigning button selection
-buttAll.forEach((button) => {
-    button.addEventListener('click', () => {
-        if (clickcount == 1) {
-    playerSelection = button.id
-    console.log(button.id);
-    game();
-        } else {
-            return 'ERROR';
-        }
-    });
-});
-        
+function stopGame() {
+    alert("The game is over. Reload to restart.");
+   
+}
 
+function updateP() {
+    document.querySelector('.pScore').textContent = "Player score = " + playerScore + "!";
+}
+function updateC() {
+    document.querySelector('.cScore').textContent = "Computer score = " + computerScore + "!";
 
+}
+function updateD() {
+    document.querySelector('.dScore').textContent = "Tied by " + drawScore + "!";
+}
+
+function displayRound() {
+    
+}
 
